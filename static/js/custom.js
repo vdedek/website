a709092aa0fdcc2478a238996b2c8ade089b85aa
vdedek <dedekviktor@gmail.com>
2024-03-18 13:32:04 +0100

    breaking JS improvement

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,9 @@
-$('.ls-block').each(function(index, value){
-    var str = $(this).html();
-    str = str.replace(/(\s|^)(a|i|k|o|s|u|v|z)(\s+)([^\p{Cc}\p{Cf}\p{zL}\p{Zp}]+)/gmi , '$1$2&nbsp;$4');
-    $(this).html(str);    
-})
+function csTypo() {
+    $('.ls-block').each(function(index, value){
+      var str = $(this).html();
+      str = str.replace(/(\s|^)(a|i|k|o|s|u|v|z)(\s+)([^\p{Cc}\p{Cf}\p{zL}\p{Zp}]+)/gmi , '$1$2&nbsp;$4');
+      $(this).html(str);    
+    })
+  }
+  
+  window.addEventListener("load", syntaxHighlights);
