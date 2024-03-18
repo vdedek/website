e1c731e02ddcf50900caf2d6644349f886ed8607
vdedek <dedekviktor@gmail.com>
2024-03-18 15:25:37 +0100

    index adjsutments, custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,9 +1,11 @@
 function csTypo() {
-    $('.ls-block').each(function(index, value){
+    $('.ls-block').each(function(index, value) {
       var str = $(this).html();
-      str = str.replace(/(\s|^)(a|i|k|o|s|u|v|z)(\s+)([^\p{Cc}\p{Cf}\p{zL}\p{Zp}]+)/gmi , '$1$2&nbsp;$4');
-      $(this).html(str);    
-    })
+      // Updated regular expression to handle "a" and other single-letter words
+      str = str.replace(/(\s|^)([aikosuvz])(\s+)([^\p{Cc}\p{Cf}\p{zL}\p{Zp}]+)/gmi, '$1$2&nbsp;$4');
+      $(this).html(str);
+    });
   }
   
-  window.addEventListener("load", syntaxHighlights);
+  window.addEventListener("load", csTypo); // Corrected function name here
+  
