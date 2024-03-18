317e356670fe2c97b21f65f2600001d9d8422a63
vdedek <dedekviktor@gmail.com>
2024-03-18 13:27:15 +0100

    js for breaking

---
+++ static/js/custom.js
@@ -0,0 +1,5 @@
+$('.ls-block').each(function(index, value){
+    var str = $(this).html();
+    str = str.replace(/(\s|^)(a|i|k|o|s|u|v|z)(\s+)([^\p{Cc}\p{Cf}\p{zL}\p{Zp}]+)/gmi , '$1$2&nbsp;$4');
+    $(this).html(str);    
+})
